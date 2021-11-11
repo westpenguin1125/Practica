9d2fd838630c37b05dd0a8633676c438a2f119cb
westpenguin1125 <westpenguin1125@gmail.com>
2021-11-11 10:46:00 +0100

    Update ShootCommand.java
    Shoot command puesto esta vez. UPS
    
    Co-Authored-By: Kevin Oscar Arce Vera <76266686+kanker2@users.noreply.github.com>

--- src/es/ucm/tp1/control/commands/ShootCommand.java
+++ src/es/ucm/tp1/control/commands/ShootCommand.java
@@ -1,5 +1,27 @@
 package es.ucm.tp1.control.commands;
 
-public class ShootCommand {
+import es.ucm.tp1.logic.Game;
+
+public class ShootCommand extends Command{
+
+	private static final String NAME = "shoot";
+
+	private static final String DETAILS = "[s]hoot";
+
+	private static final String SHORTCUT = "s";
+
+	private static final String HELP = "shoot bullet";
+
+	
+	public ShootCommand() {
+		super(NAME, SHORTCUT, DETAILS, HELP);
+
+	}
+
+	@Override
+	public boolean execute(Game game) {
+		// TODO pa la próxima vez
+		return false;
+	}
 
 }
